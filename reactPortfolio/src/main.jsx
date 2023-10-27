94a869577de1eb3fa49ddf3eb61b036f7a873b61
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-10-27 16:56:42 -0600

    testing app

--- reactPortfolio/src/main.jsx
+++ reactPortfolio/src/main.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-deprecated */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import  { App }  from './App/App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+ReactDOM.render(<App/>, document.getElementById('root'));
