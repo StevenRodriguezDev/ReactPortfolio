8925b6222df8f7e609ccf0084535aaac953332fe
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-10-30 01:07:24 -0600

    created resume page

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,12 @@
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+      <a href="/src/assets/pdf/SRCV.pdf"> 
+       Resume Link 
+       </a>
+      </p>
+    </div>
+  );
+}
