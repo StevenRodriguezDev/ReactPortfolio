b485247de6cdfdbd914b82892f6bbfb59d19b797
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-10-30 01:27:52 -0600

    updated portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,138 +1,64 @@
 /* eslint-disable react/no-unescaped-entities */
-import MenuMatch from '/src/assets/images/MenuMatchDesktop1.png'
-import CalcApp from '/src/assets/images/Calc.png'
-import Work from '/src/assets/images/Workday.png'
-import PasswordGen from '/src/assets/images/Password.png'
-import MovieDB from '/src/assets/images/MovieDB.png'
-import Employee from '/src/assets/images/directory.png'
-
+import MenuMatch from '/src/assets/images/MenuMatchDesktop1.png';
+import CalcApp from '/src/assets/images/Calc.png';
+import Work from '/src/assets/images/Workday.png';
+import PasswordGen from '/src/assets/images/Password.png';
+import MovieDB from '/src/assets/images/MovieDB.png';
+import Employee from '/src/assets/images/directory.png';
+import Project from '/src/components/Project';
 
 export default function Portfolio() {
   return (
     <main>
-      {/* <!-- <section id="portfolio"> --> */}
       <h1 id="portfolio">Portfolio</h1>
 
-      <section>
-      <h2>React Calculator</h2>
-        <p>
-          This is a 
-          <a href="https://stevenrodriguezdev.github.io/calc-app/" target="_blank"rel="noreferrer"> React Calculator</a> ,
-          enter in the numbers you want, and press the big =
-          button and get the results.
-          <a href="https://github.com/StevenRodriguezDev/Quiz-App" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a href="https://stevenrodriguezdev.github.io/calc-app/" target="_blank"rel="noreferrer" >
-        <img
-          src={CalcApp}
-          alt="calcApp"
-          className="img"
-        />
-        </a>
-        {/* <!-- <div class="middle">
-          <div class="text">Deploy</div>
-        </div> --> */}
-       
-      </section>
+      <Project
+        title="React Calculator"
+        description="This is a React Calculator, enter in the numbers you want, and press the big = button and get the results."
+        imageUrl={CalcApp}
+        projectLink="https://stevenrodriguezdev.github.io/calc-app/"
+        repoLink="https://github.com/StevenRodriguezDev/Quiz-App"
+      />
 
-      <section>
-      <h2>Menu Match</h2>
-        <p>
-          <a href="https://jkellogg01.github.io/menu-match/" target="_blank" rel="noreferrer">
-              Menu Match
-          </a>
-          is a project that is designed to streamline the process of finding a
-          recipe, shopping for ingredients, and preparing a great meal.
-          <a href="https://github.com/jkellogg01/menu-match" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a href="https://jkellogg01.github.io/menu-match/" target="_blank"rel="noreferrer" >
-        <img
-          src={MenuMatch}
-          alt="menuPage"
-          className="img"
-        />
-        </a>
-      </section>
+      <Project
+        title="Menu Match"
+        description="Menu Match is a project that is designed to streamline the process of finding a recipe, shopping for ingredients, and preparing a great meal."
+        imageUrl={MenuMatch}
+        projectLink="https://jkellogg01.github.io/menu-match/"
+        repoLink="https://github.com/jkellogg01/menu-match"
+      />
 
-      <section>
-      <h2>WorkDay Scheduler</h2>
-        <p>
-        A simple 
-        <a
-        href="https://stevenrodriguezdev.github.io/WorkDay-Scheduler/" target="_blank" rel="noreferrer">
-        calendar application </a>
-        that allows a user to save events for each hour of a typical working day (9am–5pm) This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-        <a href="https://github.com/StevenRodriguezDev/WorkDay-Scheduler" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a href="https://stevenrodriguezdev.github.io/WorkDay-Scheduler/" target="_blank" rel="noreferrer">
-        <img
-           src={Work}
-          alt="Workday"
-          className="img"
-        />
-        </a>
-      </section>
+      <Project
+        title="WorkDay Scheduler"
+        description="A simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery."
+        imageUrl={Work}
+        projectLink="https://stevenrodriguezdev.github.io/WorkDay-Scheduler/"
+        repoLink="https://github.com/StevenRodriguezDev/WorkDay-Scheduler"
+      />
 
-      <section>
-      <h2>Password Generator</h2>
-        <p>
-        This 
-        <a
-          href="https://stevenrodriguezdev.github.io/password-generator/" target="_blank" rel="noreferrer" >
-          Password Generator 
-        </a>
-        is an application that an employee can use to generate a random password based on criteria they've selected.
-        <a href="https://github.com/StevenRodriguezDev/password-generator" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a    href="https://stevenrodriguezdev.github.io/password-generator/" target="_blank" rel="noreferrer" >
-        <img 
-        src={PasswordGen}
-        alt="Password" 
-        className="img" />
-        </a>
-      </section>
+      <Project
+        title="Password Generator"
+        description="This Password Generator is an application that an employee can use to generate a random password based on criteria they've selected."
+        imageUrl={PasswordGen}
+        projectLink="https://stevenrodriguezdev.github.io/password-generator/"
+        repoLink="https://github.com/StevenRodriguezDev/password-generator"
+      />
 
-      <section>
-      <h2>Movie Database</h2>
-        <p>
-          <a  
-          href="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com" target="_blank" rel="noreferrer">
-        
-        Little Site of Horrors
-        </a> 
-        was created to delight observers of Spooky Season, young and old. 💀 By providing users with a database of scary, and not so-scary, movies to watch in October (or anytime the need arises) we're hoping everyday can be a little bit spookier!
-        <a href="https://github.com/ljkahn/littleSiteOfHorrors" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a href="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com" target="_blank" rel="noreferrer">
-        
-        <img
-           src={MovieDB}
-          alt="database of movies"
-          className="img"
-        />
-        </a>
-      </section>
+      <Project
+        title="Movie Database"
+        description="Little Site of Horrors was created to delight observers of Spooky Season, young and old. By providing users with a database of scary, and not-so-scary, movies to watch in October (or anytime the need arises), we're hoping every day can be a little bit spookier!"
+        imageUrl={MovieDB}
+        projectLink="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com"
+        repoLink="https://github.com/ljkahn/littleSiteOfHorrors"
+      />
 
-      <section>
-      <h2>Empoyee Directory</h2>
-        <p>
-          <a
-          href="https://stevenrodriguezdev.github.io/EmployeeDirectory/" target="_blank" rel="noreferrer">
-          
-          Employee Directory
-         </a>
-         is an third-party API App to see your Employees
-         <a href="https://github.com/StevenRodriguezDev/EmployeeDirectory" target="_blank"rel="noreferrer"> GitHub Repo </a>
-        </p>
-        <a  href="https://stevenrodriguezdev.github.io/EmployeeDirectory/" target="_blank" rel="noreferrer">
-        <img
-           src={Employee}
-          alt="calc-page"
-          className="img"
-        />
-        </a>
-      </section>
-      {/* <!-- </section> --> */}
+      <Project
+        title="Employee Directory"
+        description="Employee Directory is a third-party API app to see your employees."
+        imageUrl={Employee}
+        projectLink="https://stevenrodriguezdev.github.io/EmployeeDirectory/"
+        repoLink="https://github.com/StevenRodriguezDev/EmployeeDirectory"
+      />
     </main>
   );
 }
