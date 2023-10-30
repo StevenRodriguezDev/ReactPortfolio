ff64629a465d80d05a39215a2fddd4aece1e88d2
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-10-30 01:04:46 -0600

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-no-undef */
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import Footer from './components/Footer'; // Corrected the component name to PascalCase
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
@@ -10,6 +12,7 @@ function App() {
       <main className="mx-3">
         <Outlet />
       </main>
+      <Footer /> {/* Corrected the component name to PascalCase */}
     </>
   );
 }
